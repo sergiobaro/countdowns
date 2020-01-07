848b7a6bfa3f6a378fd09aa4d7308d01064168cf
Sergio Baro <serbarsim@gmail.com>
2020-01-07 09:36:29 +0100

    Added add countdown view

---
+++ countdowns/Modules/AddCountdown/View/AddCountdownView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct AddCountdownView: View {
+  
+  var body: some View {
+    NavigationView {
+      Text("Add Countdown")
+        .navigationBarTitle("Add Countdown")
+    }
+  }
+}
+
+struct AddCountdownView_Previews: PreviewProvider {
+  static var previews: some View {
+    AddCountdownView()
+  }
+}
--- countdowns/Modules/CountdownsList/View/CountdownsListView.swift
+++ countdowns/Modules/CountdownsList/View/CountdownsListView.swift
@@ -1,8 +1,36 @@
 import SwiftUI
 
 struct CountdownsListView: View {
+  
+  @State private var items = {
+    (0...30).map { "Countdown \($0)" }
+  }()
+  
+  @State private var showAddCountdown = false
+  
   var body: some View {
-    Text("Countdowns")
+    NavigationView {
+      List {
+        ForEach(self.items, id: \.self) { item in
+          NavigationLink(destination: EditCountdownView(title: item)) {
+            Text(item)
+          }
+        }
+        .onDelete(perform: { index in
+          self.items.remove(at: index.first!)
+        })
+      }
+      .navigationBarTitle("Countdowns")
+      .navigationBarItems(trailing:
+        Button(action: { self.showAddCountdown = true }) {
+          Image(systemName: "plus.circle")
+            .font(.title)
+        }
+        .sheet(isPresented: self.$showAddCountdown) {
+          AddCountdownView()
+        }
+      )
+    }
   }
 }
 
