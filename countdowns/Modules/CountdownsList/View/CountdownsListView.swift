73b8f73d8be61d81d3850d0163f87b2c8f39770b
Sergio Baro <serbarsim@gmail.com>
2020-01-07 08:56:03 +0100

    Project main folders

---
+++ countdowns/Modules/CountdownsList/View/CountdownsListView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct CountdownsListView: View {
+  var body: some View {
+    Text("Countdowns")
+  }
+}
+
+struct CountdownsListView_Previews: PreviewProvider {
+  static var previews: some View {
+    CountdownsListView()
+  }
+}
